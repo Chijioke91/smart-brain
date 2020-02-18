3633c34d495080188ff5cda52e068aabcc254d2e
= <=>
2020-02-18 12:04:10 +0100

    postgres config changes

--- utils/knex.js
+++ utils/knex.js
@@ -2,13 +2,20 @@ const knex = require('knex');
 
 const db = knex({
   client: 'pg',
-  version: '7.2',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'greatcjiano91',
-    database: 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
+// const db = knex({
+//   client: 'pg',
+//   version: '7.2',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'postgres',
+//     password: 'greatcjiano91',
+//     database: 'smart-brain'
+//   }
+// });
 
 module.exports = db;
