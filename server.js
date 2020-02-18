9297c0499ac8f6516b51470ddf4e69ae9aa1ce2b
= <=>
2020-02-18 11:11:02 +0100

    deployment changes

--- server.js
+++ server.js
@@ -11,4 +11,15 @@ app.use('/', allRoute);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server is up on port ${PORT}`));
+// app.listen(PORT, console.log(`server is up on port ${PORT}`));
+
+const server = app.listen(
+  PORT,
+  console.log(`server is running on port ${PORT}`)
+);
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  server.close(() => process.exit(1));
+});
